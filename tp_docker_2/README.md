b4a2bc380fbada87622238fddc0b2d0d34ebce44
Anorak <palimagus@outlook.fr>
2022-12-13 11:59:09 +0100

    Création de la nouvelle image & lien avec le projet

--- tp_docker_2/README.md
+++ tp_docker_2/README.md
@@ -8,6 +8,12 @@ Récupération du code source pour le TP
 
 Création d'un conteneur MariaDB
 
-`docker run --name tp-2-mariadb -e MYSQL_ROOT_PASSWORD=1234 -d mariadb`
+`docker run --name tp-2-mariadb -p 3001:3306 -e MYSQL_ROOT_PASSWORD=1234 -d mariadb`
 
 Modification du fichier de configuration source pour utiliser la nouvelle base de données
+
+Création d'une image Docker pour le code source de l'API précédemment récupérée et lancement d'un conteneur basé sur celle-ci
+
+`docker build -t api-quentin .`  
+`docker run --name tp-api --link tp-2-mariadb -p 3000:3000 api-quentin`  
+`docker exec -it tp-2-mariadb mysql -u root -p`
--- tp_docker_2/src/db.config.js
+++ tp_docker_2/src/db.config.js
@@ -1,16 +1,17 @@
 // Uncomment this block to use sqlite
-module.exports = {
-    dialect: "sqlite",
-    storage: "./my-db.sqlite",
-}
+// module.exports = {
+//     dialect: "sqlite",
+//     storage: "./my-db.sqlite",
+// }
 
 // Uncomment this block to use mysql
-/*module.exports = {
-    hostname: "",
-    username: "",
-    password: "",
-    database: "",
+module.exports = {
+    dialect: "mysql",
+    hostname: "172.17.0.2",
+    username: "root",
+    password: "1234",
+    database: "quentin",
     port: 3306
-}*/
+}
 
 // TODO : adapt this file to load parameters from environment variables (process.env.VARIABLE_NAME)
